4c2faba5023f41f0f5f83f1c5c124c7adbb94f0d
Miaoxingren <363326042@qq.com>
2017-02-23 09:30:09 +0800

    remove npc.

--- src/js/game/CameraCtrl.js
+++ src/js/game/CameraCtrl.js
@@ -11,9 +11,9 @@
         this.cameras = [];
 
         var camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
-        camera.position.x = 850;
+        camera.position.x = 50;
         camera.position.y = 50;
-        camera.position.z = 900;
+        camera.position.z = 50;
         camera.lookAt(0, 0, 0);
 
         camera.tag = lynx.enum.tag.PLAYER;
--- src/js/game/NpcCtrl.js
+++ src/js/game/NpcCtrl.js
@@ -1,3 +1,63 @@
+(function(lynx) {
+    var taskState = lynx.enum.task;
+
+    lynx.NPC = function(graph) {
+        this.graph = graph;
+        this.name = graph.name;
+        this.tasks = [];
+        this.taskIndex = -1;
+    };
+
+    var npcProto = lynx.NPC.prototype;
+
+    npcProto.addTask = function (task) {
+        if (!lynx.isArray(task)) {
+            task.state = taskState.CREATE;
+            this.tasks.push(task);
+            return;
+        }
+
+        for (var i = 0, iLen = task.length; i < iLen; i++) {
+            this.addTask(task[i]);
+        }
+    };
+
+    npcProto.checkTask = function (task) {
+        if (task.state === taskState.COMPLET) {
+            this.endTask(task);
+        }
+    };
+
+    npcProto.endTask = function (task) {
+        var list = this.tasks;
+        for(var i = 0, iLen = list.length; i < iLen; i++) {
+            if (list[i].name === task.name) {
+                task.end();
+                // task.state = taskState.COMPLET;
+                list.splice(i, 1);
+            }
+        }
+    };
+
+    npcProto.getCurTask = function () {
+        return this.tasks[this.taskIndex];
+    };
+
+    npcProto.getConversation = function () {
+        var task = this.tasks[this.taskIndex];
+        return task.messages[task.state];
+    };
+
+    npcProto.updateTask = function () {
+        var task = this.tasks[this.taskIndex];
+        if (task.state === taskState.CREATE) {
+            task.state = taskState.ACCEPT;
+        }
+    };
+
+})(lynx);
+
+
 // add npcs to paw
 (function(lynx) {
     var paw = lynx.wdConf.paw;
